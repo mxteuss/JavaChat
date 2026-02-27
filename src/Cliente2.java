c075ab26d5d8178bb2dad24bce73f824aa1f3a09
mxteuss <ovagabundant@gmail.com>
2026-02-27 17:45:48 -0300

    feat: add Cliente 1 e 2

---
+++ src/Cliente2.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente2 {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 5000);
+        System.out.println("Digite algo: ");
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            ClienteThread clienteThread = new ClienteThread(socket);
+            clienteThread.start();
+            PrintStream saida = new PrintStream(socket.getOutputStream());
+            String teclado = sc.nextLine();
+            saida.println(teclado);
+        }
+
+    }
+}
