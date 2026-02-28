09d0ad14084d9f48603b3bdeaaa687e9ab2de95b
mxteuss <ovagabundant@gmail.com>
2026-02-28 20:01:57 -0300

    feat: redirecionar clientes para salas

---
+++ src/server/ClienteHandler.java
@@ -0,0 +1,44 @@
+package server;
+
+import service.SalasService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClienteHandler extends Thread{
+
+    private final Socket socket;
+    private final SalasService salasService;
+    private String salaAtual = null;
+
+    public ClienteHandler(Socket socket, SalasService salasService) {
+        this.socket = socket;
+        this.salasService = salasService;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter saida = new PrintWriter(socket.getOutputStream(), true);
+            String x;
+
+            while ((x = br.readLine()) != null){
+                if (x.startsWith("/entrar")){
+                salaAtual = x.split(" ")[1];
+                salasService.entrarSala(salaAtual, saida);
+                } else if (salaAtual == null) {
+                    saida.println("Entre em uma sala primeiro. Use /entrar <sala>");
+                }
+                else {
+                    salasService.enviarParaSala(salaAtual, x);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
