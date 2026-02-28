ee88d2b3b3f06f07af8b7f949b91ec039392fab4
mxteuss <ovagabundant@gmail.com>
2026-02-28 20:02:47 -0300

    chore: receber clientes no mesmo server

---
+++ src/server/Main.java
@@ -0,0 +1,20 @@
+package server;
+
+import service.SalasService;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4000);
+        SalasService salasService =  new SalasService();
+
+        while (true){
+            Socket socket = serverSocket.accept();
+            System.out.println("Novo cliente entrou!");
+            new ClienteHandler(socket, salasService).start();
+        }
+
+    }
+}
